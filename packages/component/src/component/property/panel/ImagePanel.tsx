7b08c5407126a072a40d259ebf1aed534b72edab
NAKAMURA, Hisashi <hisashi@sunvisor.net>
2025-03-27 14:46:27 +0900

    Changed to use React19's `use` in ImagePanel

--- packages/component/src/component/property/panel/ImagePanel.tsx
+++ packages/component/src/component/property/panel/ImagePanel.tsx
@@ -4,7 +4,7 @@
  * Created by sunvisor on 2024/02/19.
  * Copyright (C) Sunvisor Lab. 2024.
  */
-import { useCallback, useEffect, useState } from "react";
+import { use, useCallback, useState, Suspense, useMemo } from "react";
 import PositionFields from '../fieldGroup/PositionFields';
 import SizeFields from '../fieldGroup/SizeFields';
 import { Box, Button } from '@mui/material';
@@ -23,38 +23,30 @@ type Props = {
   unit: UnitValue;
   values: ImageData;
   onChangeValue: ChangeValueHandler<ImagePanelValueType>;
-}
+};
+type InnerProps = {
+  promise: Promise<ImageListData[]>
+} & Props;
 
-export default function ImagePanel(props: Props) {
-  const { imageOptions, unit, values, onChangeValue } = props;
-  const { getImageList, getImageUrl, noImageUrl }  = imageOptions;
+// Suspense 内部で呼び出す用のラッパー
+function InnerImagePanel(
+  { imageOptions: { getImageUrl, noImageUrl }, unit, values, onChangeValue, promise }: InnerProps
+) {
   const t = translation().imageProperty;
+  const imageList = use(promise);
   const [openSelect, setOpenSelect] = useState(false);
-  const [imageList, setImageList] = useState<ImageListData[]>([]);
   const srcUrl = values.src.length ? getImageUrl(values.src) : noImageUrl;
 
-  useEffect(() => {
-    const fetchImageList = async () => {
-      const imageList = await getImageList();
-      setImageList(imageList);
-    }
-    // noinspection JSIgnoredPromiseFromCall
-    fetchImageList();
+  const handleOpenSelect = useCallback(() => {
+    setOpenSelect(true);
   }, []);
-
-  const handleOpenSelect = useCallback(
-    async () => {
-      setOpenSelect(true);
-    },
-    []
-  );
 
   const handleImageListPanelSelect = useCallback((image?: ImageListData) => {
     if (image) {
       onChangeValue('src', image.name, true);
     }
     setOpenSelect(false);
-  }, [onChangeValue, setOpenSelect]);
+  }, [onChangeValue]);
 
   return (
     <>
@@ -77,16 +69,13 @@ export default function ImagePanel(props: Props) {
               variant="outlined"
               onClick={handleOpenSelect}
               disabled={imageList.length === 0}
-              startIcon={<CollectionsIcon/>}
+              startIcon={<CollectionsIcon />}
             >
               {t.select}
             </Button>
           </Box>
           <Box sx={{ height: 200 }}>
-            <SvImage
-              src={srcUrl}
-              alt={srcUrl}
-            />
+            <SvImage src={srcUrl} alt={srcUrl} />
           </Box>
         </>
       }
@@ -98,5 +87,19 @@ export default function ImagePanel(props: Props) {
         />
       }
     </>
+  );
+}
+
+// 外から見ると従来と同じシグネチャ
+export default function ImagePanel(props: Props) {
+  const promise = useMemo(
+    () => props.imageOptions.getImageList(),
+    [props.imageOptions]
+  );
+
+  return (
+    <Suspense fallback={<div>Loading images...</div>}>
+      <InnerImagePanel promise={promise} {...props} />
+    </Suspense>
   );
 }
